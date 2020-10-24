04c8fd72f48a40b502d5d50089af8605159c2f44
takumi.hatae <plazima0131@softbank.ne.jp>
2020-10-24 21:52:45 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by plazima on 2020/10/24.
+//  Copyright © 2020 takumi.hatae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
